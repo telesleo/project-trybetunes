8d01af87d7fe8ed8e9a98c55b3325bdb779ce346
leo <leonardoandrei41@gmail.com>
2023-04-25 19:59:14 -0300

    Arruma rotas - remove AudioPlayer da tela de login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
-import NotFound from './pages/NotFound';
 import './App.css';
 import AudioPlayer from './components/AudioPlayer';
 
@@ -30,35 +29,31 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Route exact path="/" component={ Login } />
-          <Switch>
-            <Route exact path="/search" component={ Search } />
-            <Route
-              exact
-              path="/album/:id"
-              render={ (props) => (
-                <Album { ...props } updateCurrentSong={ this.updateCurrentSong } />
-              ) }
-            />
-            <Route
-              exact
-              path="/favorites"
-              render={ (props) => (
-                <Favorites { ...props } updateCurrentSong={ this.updateCurrentSong } />
-              ) }
-            />
-            <Route exact path="/profile" component={ Profile } />
-            <Route exact path="/profile/edit" component={ ProfileEdit } />
-            <Route
-              path={
-                ['/search', '/favorites', '/profile', '/profile/edit', '/album/:id']
-              }
-              render={ () => (
-                <AudioPlayer currentSongUrl={ currentSongUrl } />
-              ) }
-            />
-            <Route path="/" component={ NotFound } />
-          </Switch>
-          <AudioPlayer currentSong={ currentSong } />
+          <Route exact path="/search" component={ Search } />
+          <Route
+            exact
+            path="/album/:id"
+            render={ (props) => (
+              <Album { ...props } updateCurrentSong={ this.updateCurrentSong } />
+            ) }
+          />
+          <Route
+            exact
+            path="/favorites"
+            render={ (props) => (
+              <Favorites { ...props } updateCurrentSong={ this.updateCurrentSong } />
+            ) }
+          />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route
+            path={
+              ['/search', '/favorites', '/profile', '/profile/edit', '/album/:id']
+            }
+            render={ () => (
+              <AudioPlayer currentSong={ currentSong } />
+            ) }
+          />
         </BrowserRouter>
       </div>
     );
