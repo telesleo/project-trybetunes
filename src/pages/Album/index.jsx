389b5bcf0022b5b8a4e229ee4717b754270b8fee
leo <leonardoandrei41@gmail.com>
2023-05-09 19:42:09 -0300

    Mostra a capa do álbum selecionado

--- src/pages/Album/index.jsx
+++ src/pages/Album/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from '../components/Header';
-import getMusics from '../services/musicsAPI';
-import Loading from '../components/Loading';
-import MusicList from '../components/MusicList';
+import Header from '../../components/Header';
+import getMusics from '../../services/musicsAPI';
+import Loading from '../../components/Loading';
+import MusicList from '../../components/MusicList';
+import styles from './style.module.css';
 
 class Album extends React.Component {
   constructor() {
@@ -14,6 +15,7 @@ class Album extends React.Component {
       loading: true,
       artistName: '',
       collectionName: '',
+      artwork: '',
     };
   }
 
@@ -26,17 +28,19 @@ class Album extends React.Component {
       loading: false,
       artistName: songs[0].artistName,
       collectionName: songs[0].collectionName,
+      artwork: songs[0].artworkUrl100,
     });
   }
 
   render() {
     const { updateCurrentSong } = this.props;
-    const { songs, loading, artistName, collectionName } = this.state;
+    const { songs, loading, artistName, collectionName, artwork } = this.state;
 
     const content = (
       <>
         <h2 data-testid="album-name">{collectionName}</h2>
         <h3 data-testid="artist-name">{artistName}</h3>
+        <img className={ styles.artwork } src={ artwork } alt="Album cover" />
         <MusicList
           updateCurrentSong={ updateCurrentSong }
           songs={ songs.slice(1, songs.length) }
